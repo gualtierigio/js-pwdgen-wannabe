71e98e6237dfbcbeb171aeb24deffce83f0f4326
gualtierigio <gualtierigio@gmail.com>
2021-08-05 15:12:51 +0200

    password generetor inizzializzazione

---
+++ myscript.js
@@ -0,0 +1,11 @@
+document.getElementById("titolo").innerHTML = "ora sono un titolo migliore";
+
+
+
+var dataOfBirth = prompt("quando sei nato/a")
+
+var surname = prompt("scrivi il tuo cognome")
+
+var FavouriteColor = prompt("scrivi il tuo colore preferito")
+
+var password = (dataOfBirth + surname + FavouriteColor).innerHTML
